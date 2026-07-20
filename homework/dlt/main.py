8c1b9639a131ab970cc3972b1326ab343a1697fd
Rezky Aulia <rezkyauliapratama@gmail.com>
2026-07-20 21:11:29 +0700

    Update main.py

--- homework/dlt/main.py
+++ homework/dlt/main.py
@@ -4,7 +4,9 @@ load_dotenv()
 
 from agent import faq_agent, SearchDeps
 from ingest import build_index, load_faq_data
-
+import logfire
+logfire.configure()
+logfire.instrument_pydantic_ai()
 
 def main():
     # Download the FAQ and build the search index
@@ -15,7 +17,7 @@ def main():
     deps = SearchDeps(index=index)
 
     # Ask a question
-    question = 'I just discovered the course. Can I join it?'
+    question = 'How do I run Ollama locally?'
     result = faq_agent.run_sync(question, deps=deps)
 
     print(result.output)
